065222642a46749f4dca9ffb5f2a59c58e0b3965
Raphael Nogueira <raphael.nogueira@5by5.com.br>
2022-07-09 15:02:07 -0300

    Implementação para validar se a extensao do arquivo é a mesma do mimeType do base64

---
+++ src/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.StaticFiles;
+
+while (true)
+{
+    Console.WriteLine("Informe a extensão: ");
+    var extension = Console.ReadLine();
+
+    var base64MimeType = "data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+
+    var hasContentType = new FileExtensionContentTypeProvider().TryGetContentType($".{extension}", out var mimeContentType);
+
+    if (hasContentType && base64MimeType!.Contains(mimeContentType))
+        Console.WriteLine("A extensão é igual ao base64");
+
+    else
+        Console.WriteLine("A extensão é diferente do base64");
+}
